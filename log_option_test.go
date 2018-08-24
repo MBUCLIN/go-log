757bd9fe06fa2b884dbb7437d63d4f28b12fe924
mbuclin <mbuclin@student.42.fr>
2018-08-24 18:38:05 +0200

    create: Create log option tests #7

---
+++ log_option_test.go
@@ -0,0 +1,36 @@
+package golog
+
+import (
+  "testing"
+)
+
+// Does not really test anything, just test build.
+func  LogOptionTest(t *testing.T) {
+  t.Run("LO=Struct", func(t *testing.T) {
+    var option LogOption = LogOption{
+      "Info",
+      false,
+      false,
+      true,
+      false,
+    };
+
+    t.Logf("option: %v\n", option);
+  });
+}
+
+// Test NewLogOption function.
+func  TestNewLogOption(t * testing.T) {
+  t.Run("LO=New", func(t *testing.T) {
+    var option *LogOption = NewLogOption("Warning", true);
+
+    if option.Level != "Warning" {
+      t.Errorf("LO=New: Fail: level is not set to Warning but: %s\n", option.Level);
+      t.Failed();
+    }
+    if option.Keep != true {
+      t.Errorf("LO=New: Fail: keep boolean is not true\n");
+      t.Failed();
+    }
+  })
+}
